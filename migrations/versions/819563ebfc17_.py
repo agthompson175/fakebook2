593bacfb9bd430e9c944b43fc9a1cab933c9ae09
agthompson175 <agthompson175@gmail.com>
2021-07-14 23:18:45 -0400

    first commit

---
+++ migrations/versions/819563ebfc17_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 819563ebfc17
+Revises: ff3dc1916991
+Create Date: 2021-07-14 16:28:28.957508
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '819563ebfc17'
+down_revision = 'ff3dc1916991'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
